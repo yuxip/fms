a40e6bbe6ec76e1518b081c6c42351c7ba1beed1
tomBurton83 <tom_burton83@hotmail.com>
2014-03-10 15:56:25 -0400

    Clean up HitCluster code
    Remove unused members and methods, eliminate excessive whitespace,
    standardise layout, remove unnecessary includes or use forward
    declarations where possible

--- StRoot/StPSUTools/HitCluster.cxx
+++ StRoot/StPSUTools/HitCluster.cxx
@@ -1,188 +1,124 @@
-#include <iostream>
-#include <stdlib.h>
-#include <math.h>
+#include "StPSUTools/HitCluster.h"
 
-#include "TMath.h"
-#include "TVector2.h"
+#include <TMath.h>
+#include <TObjArray.h>
+#include <TVector2.h>
 
 #include "StEvent/StFmsHit.h"
 
-#include "TowerFPD.h"
-#include "PhotonHitFPD.h"
-#include "HitCluster.h"
-#include "Yiqun.h"
-using namespace std;
-using namespace PSUGlobals;
+#include "StPSUTools/TowerFPD.h"
+#include "StPSUTools/PhotonHitFPD.h"
+#include "StPSUTools/Yiqun.h"
 
-ClassImp(HitCluster);
-
-
-
-//HitCluster *clust2Fit;
-
-
-
-HitCluster::HitCluster()
-{
-
-  tow=0;
-  Ecutoff=.5;
+namespace PSUGlobals {
+HitCluster::HitCluster() : IsEUpdated(false), tow(0), Ecutoff(0.5) {
   Clear();
-  IsEUpdated=false;
 }
 
-
-HitCluster::~HitCluster()
-{
-  if(tow)delete tow;
-};
-
-void HitCluster::Clear(void)
-{ 
-  catag   = -1 ;
-  numbTower = nPhoton =  0 ;
-  energy  =  0 ;
-  x0 = y0 = sigmaX = sigmaY = sigmaXY = chiSquare = sigmaMin = sigmaMax = -1 ;
-  thetaAxis = -10 ;
-  for(Int_t ip=0; ip<MAX_PHOTON_PER_CLUSTER; ip++) {
-    photon[ip].Clear();
-  }
-  if(tow)delete tow;
-  tow=new TObjArray(50);
+HitCluster::~HitCluster() {
+  if (tow) {
+    delete tow;
+  }  // if
 }
 
-void HitCluster::Print(void)
-{
-  if(tow)
-    {
-      
-      for(Int_t i=0; i<tow->GetEntriesFast(); i++) 
-	{
-	  ( (TowerFPD *) tow->At(i) )->Print();
-	}
-    };
-  
-  cout << "catag=" << catag << "\t numbTower=" << numbTower << "\t nPhoton=" << nPhoton << "\t energy=" << energy ;
-  cout << "\t (" << x0 << ", " << y0 << ")\t (" << sigmaX << ", " << sigmaY << ", " << sigmaXY << ")" << endl;
-  
-  cout << "thetaAxis=" << thetaAxis << ",\t sigmaMin=" << sigmaMin << ",\t sigmaMax=" << sigmaMax << endl;
-
-  for(Int_t j=0; j<nPhoton; j++) {
-    cout << "Photon #" << j << ":\n";
-    photon[j].Print();
-  }
-  cout << "chiSquare = " << chiSquare << "\n" << endl;
-  
+void HitCluster::Clear() { 
+  catag = -1;
+  numbTower = nPhoton = 0;
+  energy = 0;
+  x0 = y0 = sigmaX = sigmaY = sigmaXY = chiSquare = sigmaMin = sigmaMax = -1;
+  thetaAxis = -10;
+  for (Int_t i(0); i < mMaxPhotonsPerCluster; ++i) {
+    photon[i].Clear();
+  }  // for
+  if (tow) {
+    delete tow;
+  }  // if
+  tow = new TObjArray(50);
 }
 
-void HitCluster::FindClusterAxis(void)
-{
+void HitCluster::FindClusterAxis() {
   Double_t dSigma2, aA, bB;
-  dSigma2 = sigmaX * sigmaX - sigmaY * sigmaY ;
-  aA = sqrt( dSigma2 * dSigma2 + 4.0 * sigmaXY * sigmaXY ) + dSigma2 ;
-  bB = 2 * sigmaXY ;
-
-	if( sigmaXY < 1e-10 ) {
-		if( aA < 1e-10 ) {
-			bB = sqrt( dSigma2 * dSigma2 + 4.0 * sigmaXY * sigmaXY ) - dSigma2 ;
-			aA = 2 * sigmaXY ;
-		}
-	}
-
-
+  dSigma2 = sigmaX * sigmaX - sigmaY * sigmaY;
+  aA = sqrt(dSigma2 * dSigma2 + 4.0 * sigmaXY * sigmaXY) + dSigma2;
+  bB = 2 * sigmaXY;
+	if (sigmaXY < 1e-10) {
+		if (aA < 1e-10) {
+			bB = sqrt(dSigma2 * dSigma2 + 4.0 * sigmaXY * sigmaXY) - dSigma2;
+			aA = 2 * sigmaXY;
+		}  // if
+	}  // if
 	thetaAxis = atan2(bB, aA);
-
-
-	Double_t myPi = TMath::Pi() ; 
-	while( thetaAxis > (myPi / 2.0) )
+	Double_t myPi = TMath::Pi(); 
+	while (thetaAxis > (myPi / 2.0)) {
 		thetaAxis -= myPi;
-
-	while( thetaAxis < -(myPi / 2.0) )
+	}  // while
+	while (thetaAxis < -(myPi / 2.0)) {
 		thetaAxis += myPi;
-
-	sigmaMin = GetSigma(thetaAxis) ;
-	sigmaMax = GetSigma(thetaAxis - TMath::Pi() / 2.0) ;
-	//cout<<"sigmaMin="<<sigmaMin<<" sigmaMax="<<sigmaMax<<endl;
+	}  // while
+	sigmaMin = GetSigma(thetaAxis);
+	sigmaMax = GetSigma(thetaAxis - TMath::Pi() / 2.0);
 }
 
-
-
-// calculate sigma w.r.t the axis going through the "center" and of an angle "theta" in x-y plane
-//
-Double_t HitCluster::GetSigma(Double_t theta){
-
-	Double_t sigma = 0 ;
-
+// Calculate sigma w.r.t the axis going through the "center" and of an angle
+// "theta" in x-y plane
+Double_t HitCluster::GetSigma(Double_t theta) {
+	Double_t sigma = 0;
 	// 2-d vector vaxis define the axis
-	//
-	TVector2 vaxis( cos(theta), sin(theta) );
-
+	TVector2 vaxis(cos(theta), sin(theta));
 	// loop over all towers pointer in cluster
-	//
 	TowerFPD * oneTower;
 	float wnew =0;
-	for(Int_t it=0; it<tow->GetEntriesFast(); it++) {
-
+	for (Int_t it=0; it<tow->GetEntriesFast(); it++) {
 		oneTower = (TowerFPD *) tow->At(it);
 		// the 2-d vector from the "center" of cluster to tower
 		// "center" are at 0.5, 1.5, etc! Need shift of 0.5
-		//
-		TVector2 v1( oneTower->column() - 0.5 - x0, oneTower->row() - 0.5 - y0 );
-		
-		// perpendicular distance to the axis = length of the component of vector "v1" that is norm to "vaxis"
-		//
+		TVector2 v1(oneTower->column() - 0.5 - x0, oneTower->row() - 0.5 - y0);
+		// perpendicular distance to the axis = length of the component of vector
+		// "v1" that is norm to "vaxis"
 		Double_t dis = (v1.Norm(vaxis)).Mod();
-		
 		// contribution to sigma
-		//
-		//sigma += oneTower->energy * dis * dis ;
-		float wtmp = log(oneTower->hit()->energy() + 1-Ecutoff)>0 ? log(oneTower->hit()->energy() +1.-Ecutoff) : 0;
-		wnew    += wtmp ;
-		sigma += wtmp * dis * dis ;
-	}
-
-	//cout<<"here1 "<<(wnew>0? sqrt(sigma/wnew) : 0)<<endl;
-	return wnew>0? sqrt(sigma/wnew) : 0;
+		//sigma += oneTower->energy * dis * dis;
+		float wtmp = log(oneTower->hit()->energy() + 1. - Ecutoff) > 0 ?
+		             log(oneTower->hit()->energy() + 1. - Ecutoff) : 0;
+		wnew += wtmp;
+		sigma += wtmp * dis * dis;
+	}  // for
+	return wnew > 0 ? sqrt(sigma / wnew) : 0;
 }
-void HitCluster::CalClusterMoment(Float_t Ecoff)
-{
+
+void HitCluster::CalClusterMoment(Float_t Ecoff) {
   Ecutoff=Ecoff;
   Float_t w0, w1, mtmp, mx, my, sigx, sigy, sigXY;
-  w0 = w1 = mtmp = mx = my = sigx = sigy = sigXY = 0 ;
-	
+  w0 = w1 = mtmp = mx = my = sigx = sigy = sigXY = 0;
   TowerFPD * oneTow;
   TIter next(tow);
-  while(oneTow=(TowerFPD*) next())
-    {
-      Float_t xxx, yyy;
-      xxx = oneTow->column() - 0.5 ;
-      yyy = oneTow->row() - 0.5 ;
-      mtmp = log(oneTow->hit()->energy()+1.-Ecoff)>0 ? log(oneTow->hit()->energy()+1.-Ecoff) : 0;
-      w1 += mtmp;
-      w0    += oneTow->hit()->energy() ;
-      mx    += mtmp * xxx ;
-      my    += mtmp * yyy ;
-      sigx  += mtmp * xxx * xxx ;
-      sigy  += mtmp * yyy * yyy ;
-      sigXY += mtmp * xxx * yyy ;
-    }
-	
-  energy  = w0 ;
-  if(w1>0)
-    {
-      x0      = mx / w1 ;
-      y0      = my / w1 ;
-      sigmaX  = sqrt( fabs(sigx / w1 - x0 * x0 ) ) ;
-      sigmaY  = sqrt( fabs(sigy / w1 - y0 * y0 ) ) ;
-      sigmaXY = sigXY / w1 - x0 * y0 ;
-    }
-  else
-    {
-      x0      = 0 ;
-      y0      = 0 ;
-      sigmaX  = 0 ;
-      sigmaY  = 0 ;
-      sigmaXY = 0 ;
-    }
-  
-};
+  while (oneTow=(TowerFPD*) next()) {
+    Float_t xxx, yyy;
+    xxx = oneTow->column() - 0.5;
+    yyy = oneTow->row() - 0.5;
+    mtmp = log(oneTow->hit()->energy() + 1. - Ecoff) > 0 ?
+           log(oneTow->hit()->energy() + 1. - Ecoff) : 0;
+    w1 += mtmp;
+    w0 += oneTow->hit()->energy();
+    mx += mtmp * xxx;
+    my += mtmp * yyy;
+    sigx += mtmp * xxx * xxx;
+    sigy += mtmp * yyy * yyy;
+    sigXY += mtmp * xxx * yyy;
+  }  // while
+  energy = w0;
+  if (w1 > 0) {
+    x0 = mx / w1;
+    y0 = my / w1;
+    sigmaX = sqrt(fabs(sigx / w1 - x0 * x0));
+    sigmaY = sqrt(fabs(sigy / w1 - y0 * y0));
+    sigmaXY = sigXY / w1 - x0 * y0;
+  } else {
+    x0 = 0;
+    y0 = 0;
+    sigmaX = 0;
+    sigmaY = 0;
+    sigmaXY = 0;
+  }  // if
+}
+}  // namespace PSUGlobals
--- StRoot/StPSUTools/HitCluster.h
+++ StRoot/StPSUTools/HitCluster.h
@@ -1,27 +1,11 @@
-////////////////////////////////////////////////////////////////////
-//
-//  definition of a HitCluster (cluster of FPD towers)
-//
-///////////////////////////////////////////////////////////////////
-
 #ifndef HITCLUSTER_H
 #define HITCLUSTER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <stdlib.h>
-#include <math.h>
+#include <TObject.h>
 
-#include "TMath.h"
-#include "TVector2.h"
+#include "StPSUTools/PhotonHitFPD.h"
 
-#include "TObjArray.h"
-#include "TMinuit.h"
-#include "TVector3.h"
-
-#include "PhotonHitFPD.h"
-#define MAX_PHOTON_PER_CLUSTER 2
+class TObjArray;
 
 /*
  http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml#Copy_Constructors
@@ -43,54 +27,54 @@
 namespace PSUGlobals {//$NMSPC
 enum EClusterCategory {
   kAmbiguousCluster = 0,  // Could be 1- or 2-photon, needs to be fitted
-  k1PhotonCluster = 1,
-  k2PhotonCluster = 2,
+  k1PhotonCluster = 1,  // A cluster created by 1 photon
+  k2PhotonCluster = 2,  // A cluster created by 2 photons
   kInvalidClusterCategory
-};
+};  // enum EClusterCategory
+
+/**
+ A cluster of FMS towers
+ */
 class HitCluster : public TObject {
-
  public:
-
-  PhotonHitFPD   photon[MAX_PHOTON_PER_CLUSTER];     // Photon-Hits in the cluster
-
-  Int_t          catag  ;               // catagory of cluster (1: 1-photon,  2: 2-photon,  0: could be either 1- or 2-photon)
-  Int_t          numbTower;             // number of non_zero towers in the cluster
-  Int_t          nPhoton;               // number of photons contained in this cluster: ( nPhoton <= MAX_PHOTON_PER_CLUSTER ! )
-  Int_t          index;                 // cluster number in an event, counts from 0
-  Float_t        energy ;               // total energy  contained in this cluster (0th moment)
-  
-  Float_t        x0     ;               // mean x ("center of gravity") in local grid coordinate (1st moment)
-  Float_t        y0     ;               // mean y ("center of gravity") in local grid coordinate (1st moment)
-  Float_t        sigmaX ;               // 2nd moment in x
-  Float_t        sigmaY ;               // 2nd moment in y
-  Float_t        sigmaXY  ;             // 2nd moment in x-y
-  Float_t        thetaAxis;             // theta angle in x-y plane that define the direction of least-2nd-sigma axis
-  Float_t        sigmaMin;              // 2nd sigma w.r.t to the least-2nd-sigma axis
-  Float_t        sigmaMax;              // 2nd sigma w.r.t to the axis orthogonal to the least-2nd-sigma axis
-  Float_t        chiSquare;             // Chi-square of the fitting
-  
-  TObjArray *    tow    ;               //!  TowerFPD objects that make the cluster
-  void CalClusterMoment(Float_t Ecoff);
-  HitCluster() ;
+  HitCluster();
   ~HitCluster();
+  void CalClusterMoment(Float_t Ecoff);
+  void Clear();
+  void FindClusterAxis(Float_t Ecoff);
+  static const int mMaxPhotonsPerCluster = 2;
+  PhotonHitFPD photon[mMaxPhotonsPerCluster];  ///< Photon-Hits in the cluster
   Bool_t IsEUpdated;
-  void Clear(void) ;
-  
-  void Print(void);
-  
-  void FindClusterAxis(void);
-  void FindClusterAxis(Float_t Ecoff){Ecutoff=Ecoff;FindClusterAxis();};
-  
-  Double_t GetSigma(Double_t theta);
-  Float_t GetLastEnergySum(Int_t nclu);
-  Int_t IEW;
-  Int_t NSTB;
+  Int_t catag;  ///< catagory of cluster (see EClusterCategory)
+  Int_t numbTower;  ///< number of non_zero towers in the cluster
+  Int_t nPhoton;  ///< number of photons contained in this cluster
+  Int_t index;  ///< cluster number in an event, counts from 0
+  Float_t energy;  ///< total energy  contained in this cluster (0th moment)
+  Float_t x0;  ///< mean x in local grid coordinate (1st moment)
+  Float_t y0;  ///< mean y in local grid coordinate (1st moment)
+  Float_t sigmaX;  ///< 2nd moment in x
+  Float_t sigmaY;  ///< 2nd moment in y
+  Float_t sigmaXY;  ///< 2nd moment in x-y
+  Float_t thetaAxis;  ///< theta angle in x-y plane that define the direction
+                      ///< of least-2nd-sigma axis
+  Float_t sigmaMin;  ///< 2nd sigma w.r.t to the least-2nd-sigma axis
+  Float_t sigmaMax;  ///< 2nd sigma w.r.t to the axis orthogonal to the
+                     ///< least-2nd-sigma axis
+  Float_t chiSquare;  ///< Chi-square of the fitting
+  TObjArray* tow;  //!<  TowerFPD objects that make the cluster
+
  private:
   DISALLOW_COPY_AND_ASSIGN(HitCluster);
+  void FindClusterAxis();
+  Double_t GetSigma(Double_t theta);
   Float_t Ecutoff;
   Bool_t EnergyUpdated;
-  ClassDef (HitCluster, 7);
-  
-};
+  ClassDef(HitCluster, 7)
+};  // class HitCluster
+
+inline void HitCluster::FindClusterAxis(Float_t Ecoff) {
+  Ecutoff = Ecoff;
+  FindClusterAxis();
 }
+}  // namespace PSUGlobals
 #endif
