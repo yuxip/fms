ca04a87aba6539262c4449c07fcf8e537a9e7070
tomBurton83 <tom_burton83@hotmail.com>
2014-03-11 11:56:42 -0400

    Make PhotonHitFPD a struct
    As it now has only simple data members and very little functionality,
    change PhotonHitFPD from a class to a struct. Even though there is no
    functional effect here, it follows the convention of using struct for
    simple data-only objects.

--- StRoot/StPSUTools/PhotonHitFPD.h
+++ StRoot/StPSUTools/PhotonHitFPD.h
@@ -7,8 +7,7 @@ namespace PSUGlobals {//$NMSPC
 /**
  Definition of a photon hit (SMD position info and reconstructed energy)
  */
-class PhotonHitFPD : public TObject {
- public:
+struct PhotonHitFPD : public TObject {
   Float_t energy;  ///< Fitted energy
   Float_t errEne;  ///< Energy fit error
   Float_t xPos;  ///< Fitted (relative) x-position
@@ -16,8 +15,7 @@ class PhotonHitFPD : public TObject {
   Float_t yPos;  ///< Fitted (relative) y-position
   Float_t errYPos;  ///< y-position fit error
   /** Constructor with optional position and energy */
-  PhotonHitFPD(const Float_t x = -1., const Float_t y = -1.,
-               const Float_t e = 0.);
+  PhotonHitFPD(Float_t x = -1., Float_t y = -1., Float_t e = 0.);
   /** Destructor */
   ~PhotonHitFPD() { }
   /** Set (x, y) position and energy */
