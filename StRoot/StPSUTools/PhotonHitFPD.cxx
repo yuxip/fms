849432de8f5e9d2ed07b2d198c970b49c6c7b642
tomBurton83 <tom_burton83@hotmail.com>
2014-03-11 11:35:51 -0400

    Remove unused PhotonHitFPD members and methods
    This includes all SMD-related members

--- StRoot/StPSUTools/PhotonHitFPD.cxx
+++ StRoot/StPSUTools/PhotonHitFPD.cxx
@@ -5,61 +5,17 @@
 
 using namespace PSUGlobals;
 
-ClassImp(PhotonHitFPD);
-
 PhotonHitFPD::PhotonHitFPD() {
   Clear();
 }
 
-TVector3 PhotonHitFPD::xyz(Float_t zPos) {
-  return TVector3(xPos,yPos,zPos);
-}
-
 PhotonHitFPD::PhotonHitFPD(const Float_t x, const Float_t y, const Float_t e) {
-  energy = e ;
-  xPos   = x ;
-  yPos   = y ;
+  energy = e;
+  xPos = x;
+  yPos = y;
 }
 
 void PhotonHitFPD::Clear(void) {
-  nSmdMatch = -1;
-  smdXCen = smdXSum = smdXSig = smdYCen = smdYSum = smdYSig = -1;
   energy = 0;
   errEne = xPos = errXPos = yPos = errYPos = -1 ;
-}
-
-void PhotonHitFPD::Set(const Float_t x, const Float_t y, const Float_t e) {
-  energy = e ;
-  xPos   = x ;
-  yPos   = y ;
-}
-
-void PhotonHitFPD::SetSMD(const Float_t smd[6]) {
-  smdXCen   = smd[0]  ;
-  smdXSum   = smd[1]  ;
-  smdXSig   = smd[2]  ;
-  smdYCen   = smd[3]  ;
-  smdYSum   = smd[4]  ;
-  smdYSig   = smd[5]  ;
-}
-
-// calculate the distance between "this" and another "hit"
-Float_t PhotonHitFPD::Distance(const PhotonHitFPD& hit) {
-  Float_t dx = xPos - hit.xPos;
-  Float_t dy = yPos - hit.yPos;
-  return sqrt(dx * dx + dy* dy) ;
-}
-
-Float_t PhotonHitFPD::WeighterDistance(const PhotonHitFPD& hit) {
-  Float_t dx = xPos - hit.xPos;
-  Float_t dy = yPos - hit.yPos;
-  Float_t sumE = energy + hit.energy ;
-  Float_t weight = 4 * energy * hit.energy / (sumE * sumE);
-  return sqrt(dx * dx + dy* dy) * weight;
-}
-
-void PhotonHitFPD::Print() {
-  std::cout << "xPos:" << xPos << "(" << errXPos << ")\t yPos:";
-  std::cout << yPos << "(" << errYPos << ")\t " << "energy: " << energy <<"("
-    << errEne << ")" << "\n";
 }
--- StRoot/StPSUTools/PhotonHitFPD.h
+++ StRoot/StPSUTools/PhotonHitFPD.h
@@ -2,7 +2,6 @@
 #define PHOTONHITFPD_H
 
 #include <TObject.h>
-#include <TVector3.h>
 
 namespace PSUGlobals {//$NMSPC
 /**
@@ -10,54 +9,21 @@ namespace PSUGlobals {//$NMSPC
  */
 class PhotonHitFPD : public TObject {
  public:
-  Int_t nSmdMatch;  ///< -1: no SMD data, 0: not matched, 1: matched  
-  Float_t smdXCen;
-  Float_t smdXSum;
-  Float_t smdXSig;
-  Float_t smdYCen;
-  Float_t smdYSum;
-  Float_t smdYSig;
   Float_t energy;  ///< Fitted energy
   Float_t errEne;  ///< Energy fit error
   Float_t xPos;  ///< Fitted (relative) x-position
   Float_t errXPos;  ///< x-position fit error
   Float_t yPos;  ///< Fitted (relative) y-position
   Float_t errYPos;  ///< y-position fit error
-  TVector3 xyz(Float_t zPos = 0);
   /** Default constructor */
   PhotonHitFPD();
   /** Constructor with position and energy */
   PhotonHitFPD(const Float_t x, const Float_t y, const Float_t e);
   /** Destructor */
-  ~PhotonHitFPD() {};
+  ~PhotonHitFPD() { }
   /** Reset all values */
   void Clear();
-  /** Print values to standard output */
-  void Print();
   /** Set (x, y) position and energy */
-  void Set(const Float_t x, const Float_t y, const Float_t e);
-  void SetSMD(const Float_t smd[6]);
-  Float_t Distance(const PhotonHitFPD& hit);
-  Float_t WeighterDistance(const PhotonHitFPD& hit);
-  PhotonHitFPD& operator=(const PhotonHitFPD& rhs) {
-    if (this != &rhs) {
-      nSmdMatch = rhs.nSmdMatch;
-      smdXCen = rhs.smdXCen;
-      smdXSum = rhs.smdXSum;
-      smdXSig = rhs.smdXSig;
-      smdYCen = rhs.smdYCen;
-      smdYSum = rhs.smdYSum;
-      smdYSig = rhs.smdYSig;
-      energy = rhs.energy;
-      errEne = rhs.errEne;
-      xPos = rhs.xPos;
-      errXPos = rhs.errXPos;
-      yPos = rhs.yPos;
-      errYPos = rhs.errYPos;
-    }  // if
-    return *this;
-  }
- private:
   ClassDef(PhotonHitFPD, 4)
 };  // class PhotonHitFPD
 }  // namespace PSUGlobals
