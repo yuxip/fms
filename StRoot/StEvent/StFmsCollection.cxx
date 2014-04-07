8e760c372c72b29dc0558b08617e063e6c905adb
tomBurton83 <tom_burton83@hotmail.com>
2014-04-07 15:56:44 -0400

    Clean up StFmsCollection

--- StRoot/StEvent/StFmsCollection.cxx
+++ StRoot/StEvent/StFmsCollection.cxx
@@ -18,29 +18,54 @@
 
 static const char rcsid[] = "$Id: StFmsCollection.cxx,v 2.1 2010/01/08 22:42:30 ullrich Exp $";
 
-ClassImp(StFmsCollection)
-
 StFmsCollection::StFmsCollection() { /* no op */ }
 
 StFmsCollection::~StFmsCollection() { /* no op */ }
     
-unsigned int
-StFmsCollection::numberOfHits() const { return mHits.size(); }
+unsigned int StFmsCollection::numberOfHits() const {
+  return mHits.size();
+}
 
-void
-StFmsCollection::addHit(StFmsHit* hit){mHits.push_back(hit);}
+unsigned int StFmsCollection::numberOfClusters() const {
+  return mClusters.size();
+}
 
-StSPtrVecFmsHit&
-StFmsCollection::hits() {return mHits;}
+unsigned int StFmsCollection::numberOfPoints() const {
+  return mPoints.size();
+}
 
-const StSPtrVecFmsHit&
-StFmsCollection::hits() const {return mHits;}
+void StFmsCollection::addHit(StFmsHit* hit) {
+  mHits.push_back(hit);
+}
 
-unsigned int StFmsCollection::nClusters() const { return mClusters.size(); }
-//unsigned int StFmsCollection::nPoints() const { return mPoints.size(); }
-void StFmsCollection::addCluster(StFmsCluster* cluster){mClusters.push_back(cluster);}
-//void StFmsCollection::addPoint(StFmsPoint* point){mPoints.push_back(point);}
-StSPtrVecFmsCluster& StFmsCollection::clusters() {return mClusters;}
-const StSPtrVecFmsCluster& StFmsCollection::clusters() const {return mClusters;}
-StSPtrVecFmsPoint& StFmsCollection::points() {return mPoints;}
-const StSPtrVecFmsPoint& StFmsCollection::points() const {return mPoints;}
+void StFmsCollection::addCluster(StFmsCluster* cluster) {
+  mClusters.push_back(cluster);
+}
+
+void StFmsCollection::addPoint(StFmsPoint* point) {
+  mPoints.push_back(point);
+}
+
+StSPtrVecFmsHit& StFmsCollection::hits() {
+  return mHits;
+}
+
+const StSPtrVecFmsHit& StFmsCollection::hits() const {
+  return mHits;
+}
+
+StSPtrVecFmsCluster& StFmsCollection::clusters() {
+  return mClusters;
+}
+
+const StSPtrVecFmsCluster& StFmsCollection::clusters() const {
+  return mClusters;
+}
+
+StSPtrVecFmsPoint& StFmsCollection::points() {
+  return mPoints;
+}
+
+const StSPtrVecFmsPoint& StFmsCollection::points() const {
+  return mPoints;
+}
--- StRoot/StEvent/StFmsCollection.h
+++ StRoot/StEvent/StFmsCollection.h
@@ -18,38 +18,57 @@
 #define StFmsCollection_hh
 
 #include "StObject.h"
-#include "StContainers.h"
+#include "StContainers.h"  // StSPtrVecFmsHit/Cluster/Point definitions
 
 class StFmsHit;
 class StFmsCluster;
-//class StFmsPoint;
+class StFmsPoint;
 
+/**
+ Collection of all hits (towers), clusters and points (photons) in the FMS.
+ 
+ This collection owns all these objects, and is itself owned by StEvent.
+ It is therefore vital to *not* delete any of the objects stored in this
+ container yourself - the collection will handle freeing memory.
+ Similarly, any object added to the collection via an add() method must be
+ allocated with new, and not be owned anywhere else.
+ */
 class StFmsCollection : public StObject {
-public:
-    StFmsCollection();
-    ~StFmsCollection();
+ public:
+  /** Constructor */
+  StFmsCollection();
+  /** Destructor. Deletes all hits, clusters and points */
+  ~StFmsCollection();
+  /** Add a hit to the collection */
+  void addHit(StFmsHit*);
+  /** Add a cluster to the collection */
+  void addCluster(StFmsCluster*);
+  /** Add a point to the collection */
+  void addPoint(StFmsPoint*);
+  /** Return the number of hits in the collection */
+  unsigned int numberOfHits() const;
+  /** Return the number of clusters in the collection */
+  unsigned int numberOfClusters() const;
+  /** Return the number of points in the collection */
+  unsigned int numberOfPoints() const;
+  /** Return the hit list */
+  StSPtrVecFmsHit& hits();
+  /** \overload */
+  const StSPtrVecFmsHit& hits() const;
+  /** Return the cluster list */
+  StSPtrVecFmsCluster& clusters();
+  /** \overload */
+  const StSPtrVecFmsCluster& clusters() const;
+  /** Return the cluster list */
+  StSPtrVecFmsPoint& points();
+  /** \overload */
+  const StSPtrVecFmsPoint& points() const;
     
-    void          addHit(StFmsHit*);
-    unsigned int  numberOfHits() const;
-
-    void          addCluster(StFmsCluster*);
-    //void          addPoint(StFmsPoint*);
-    unsigned int  nClusters() const;
-    //unsigned int  nPoints() const;
-    
-    StSPtrVecFmsHit&              hits();
-    const StSPtrVecFmsHit&        hits() const;
-    
-    StSPtrVecFmsCluster&        clusters();
-    const StSPtrVecFmsCluster&  clusters() const;
-    StSPtrVecFmsPoint&          points();
-    const StSPtrVecFmsPoint&    points() const;
-    
-private:
-    StSPtrVecFmsHit mHits;
-    StSPtrVecFmsCluster mClusters;
-    StSPtrVecFmsPoint mPoints;
-    
-    ClassDef(StFmsCollection,1)
+ private:
+  StSPtrVecFmsHit mHits;  ///< Owns all FMS hits
+  StSPtrVecFmsCluster mClusters;  ///< Owns all FMS clusters
+  StSPtrVecFmsPoint mPoints;  ///< Owns all FMS points (photons)
+  ClassDef(StFmsCollection, 2)
 };
+
 #endif
