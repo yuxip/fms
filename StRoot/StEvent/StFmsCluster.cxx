cc7b14386f177c2686a42f7be167554bb3809ca5
tomBurton83 <tom_burton83@hotmail.com>
2014-04-07 15:41:31 -0400

    Clean up StFmsCluster
    Remove unused methods, add Doxygen comments and tidy formatting

--- StRoot/StEvent/StFmsCluster.cxx
+++ StRoot/StEvent/StFmsCluster.cxx
@@ -1,61 +1,37 @@
 #include "StFmsCluster.h"
+
 #include "StMessMgr.h"
 
 #define MAX_PHOTON_PER_CLUSTER 2
 
-ClassImp(StFmsCluster)
-
-StFmsCluster::StFmsCluster():StObject() {
-
-        mCatag           = 0;
-        mNumbTower       = 0;
-        mNphoton         = 0;
-        mEnergy          = 0;
-        mX0              = 0;
-        mY0              = 0;
-        mSigmaMax        = 0;
-        mSigmaMin        = 0;
-        mCluId           = 0;
-        mChi2NdfPh1      = -1;
-        mChi2NdfPh2      = -1;
-}
+StFmsCluster::StFmsCluster()
+    : StObject(), mCatag(0), mNumbTower(0), mNphoton(0), mEnergy(0.), mX0(0.),
+      mY0(0.), mSigmaMax(0.), mSigmaMin(0.), mChi2NdfPh1(-1.), mChi2NdfPh2(-1.),
+      mCluId(0) {}
 
 StFmsCluster::~StFmsCluster() {
 }
 
-void StFmsCluster::Clear( const char* opt ) {
-
+void StFmsCluster::Print(Option_t* /* not used */) const {
+	LOG_INFO << "========StFmsCluster:\n\tcatag:\t" << GetCatag()
+           << "\n\tnumberTower:\t" << GetNTower()
+           << "\n\tnPhoton:\t" << GetNphoton()
+           << "\n\tcluster energy:\t" << GetEnergy()
+           << "\n\tx0:\t" << GetX0()
+           << "\n\ty0:\t" << GetY0()
+           << "\n\tsiggmaMax:\t" << GetSigmaMax()
+           << "\n\tsigmaMin:\t" << GetSigmaMin()
+           << "\n\tchi2NdfPh1:\t" << GetChi2NdfPh1()
+           << "\n\tchi2NdfPh2:\t" << GetChi2NdfPh2()
+           << "\n\tid:\t" << GetClusterId()
+           << "\n\tPhoton List:  \n" << endm;
 }
 
-ostream& operator<<(ostream& os, const StFmsCluster& v){
-	
-	os << "========StFmsCluster:\n\tcatag:\t"<<v.GetCatag()
-                   << "\n\tnumberTower:\t"<<v.GetNTower()
-                   << "\n\tnPhoton:\t"<<v.GetNphoton()
-                   << "\n\tcluster energy:\t"<<v.GetEnergy()
-                   << "\n\tx0:\t"<<v.GetX0()
-                   << "\n\ty0:\t"<<v.GetY0()
-                   << "\n\tsiggmaMax:\t"<<v.GetSigmaMax()
-                   << "\n\tsigmaMin:\t"<<v.GetSigmaMin()
-                   << "\n\tchi2NdfPh1:\t"<<v.GetChi2NdfPh1()
-                   << "\n\tchi2NdfPh2:\t"<<v.GetChi2NdfPh2()
-                   << "\n\tid:\t"<<v.GetClusterId()
-                   << "\n\tPhoton List:  \n";
-	return os;
-}
-
-void StFmsCluster::Print(Option_t *option) const {
-
-	LOG_INFO << *this << endm;
-}
-
-Bool_t StFmsCluster::SetNphoton( Int_t nPhoton ){
-
-	if(nPhoton<=0||nPhoton>MAX_PHOTON_PER_CLUSTER){
-		LOG_ERROR << "StFmsCluster::SetNphoton() illegal nPhoton " << endm;
+Bool_t StFmsCluster::SetNphoton(Int_t nPhoton) {
+	if (nPhoton <= 0 || nPhoton > MAX_PHOTON_PER_CLUSTER) {
+		LOG_ERROR << "StFmsCluster::SetNphoton() illegal nPhoton" << endm;
 		return false;
-	}
-	
+	}  // if
 	mNphoton = nPhoton;
 	return true;
 }
--- StRoot/StEvent/StFmsCluster.h
+++ StRoot/StEvent/StFmsCluster.h
@@ -1,88 +1,98 @@
+//Yuxi Pan 03/31/2013
 #ifndef StStFmsCluster_HH
 #define StStFmsCluster_HH
 
+#include <TLorentzVector.h>
+
 #include "StObject.h"
-#include "Stiostream.h"
-#include "TLorentzVector.h"
 
-#include "StEvent/StContainers.h"
-#include "StEvent/StFmsHit.h"
-#include "StEvent/StFmsPoint.h"
+#include "StEvent/StContainers.h"  // StPtrVecFmsHit, StPtrVecFmsPoint
 
-#include <vector>
-
-#ifndef ST_NO_NAMESPACES
-using std::vector;
-#endif
-
-#define MAX_PHOTON_PER_CLUSTER 2
-
-//class that represents a tower cluster
-//Yuxi Pan 03/31/2013
-
+/** Represents a cluster of a group of towers */
 class StFmsCluster : public StObject {
-	
-public: 
-	StFmsCluster();
-	~StFmsCluster();
-	
-	void Print( Option_t *option = "" ) const;
-	void Clear( const char* opt = "" );	
-
-	Int_t GetNstb()		const { return mNstb; }
-	Int_t GetCatag()	const { return mCatag; }
-	Int_t GetNTower()	const { return mNumbTower; }
-	Int_t GetNphoton()	const { return mNphoton; }
-	Float_t GetEnergy()	const { return mEnergy; }
-	Float_t GetX0()		const { return mX0; }
-	Float_t GetY0()		const { return mY0; }
-	Float_t GetSigmaMax()	const { return mSigmaMax; }
-	Float_t GetSigmaMin()	const { return mSigmaMin; }
-	Float_t GetChi2NdfPh1()	const { return mChi2NdfPh1; }
-	Float_t GetChi2NdfPh2()	const { return mChi2NdfPh2; }
-	Int_t GetClusterId()	const { return mCluId; }
-	TLorentzVector GetFourMomentum() const { return mFourMomentum; }
-	void SetNstb( Int_t nstb )		{ mNstb = nstb; }	
-	void SetCatag( Int_t catag )		{ mCatag = catag; }
-	void SetNumbTower( Int_t numbTower )	{ mNumbTower = numbTower; }
-	Bool_t SetNphoton( Int_t nPhoton );
-	void SetClusterEnergy( Float_t energy ) { mEnergy = energy; }
-	void SetX0 ( Float_t x0 )		{ mX0 = x0; }
-	void SetY0 ( Float_t y0 )		{ mY0 = y0; }
-	void SetSigmaMax ( Float_t sigmaMax )	{ mSigmaMax = sigmaMax; }
-	void SetSigmaMin ( Float_t sigmaMin )	{ mSigmaMin = sigmaMin; }
-	void SetChi2NdfPh1(Float_t chi2ndfph1)	{ mChi2NdfPh1 = chi2ndfph1; }
-	void SetChi2NdfPh2(Float_t chi2ndfph2)	{ mChi2NdfPh2 = chi2ndfph2; }
-	void SetClusterId ( Float_t cluid )	{ mCluId = cluid; }
-	void SetFourMomentum ( TLorentzVector p4 ) { mFourMomentum = p4; }
+ public:
+  /** Constructor */
+  StFmsCluster();
+  /** Destructor */
+  ~StFmsCluster();
+  /** Print cluster information to LOG_INFO */
+  void Print(Option_t* optionNotUsed = "") const;
+  /** Sub-detector */
+  Int_t GetNstb() const { return mNstb; }
+  /** Cluster category (see EClusterCategory) */
+  Int_t GetCatag() const { return mCatag; }
+  /** Number of towers (hits) in this cluster */
+  Int_t GetNTower() const { return mNumbTower; }
+  /** Number of points/photons forming this cluster */
+  Int_t GetNphoton() const { return mNphoton; }
+  /** Total cluster energy */
+  Float_t GetEnergy() const { return mEnergy; }
+  /** Mean x ("center of gravity") in local grid coordinate (1st moment) */
+  Float_t GetX0() const { return mX0; }
+  /** Mean y ("center of gravity") in local grid coordinate (1st moment) */
+  Float_t GetY0() const { return mY0; }
+  /** Maximum 2nd moment (along major axis) */
+  Float_t GetSigmaMax() const { return mSigmaMax; }
+  /** Minimum 2nd moment */
+  Float_t GetSigmaMin() const { return mSigmaMin; }
+  /** chi2 / ndf for 1-photon fit */
+  Float_t GetChi2NdfPh1() const { return mChi2NdfPh1; }
+  /** chi2 / ndf for 2-photon fit */
+  Float_t GetChi2NdfPh2() const { return mChi2NdfPh2; }
+  /** Eventwise cluster ID, also include nstb info. */
+  Int_t GetClusterId() const { return mCluId; }
+  /** Cluster four-momentum (px, py, pz E) */
+  TLorentzVector GetFourMomentum() const { return mFourMomentum; }
+  /** Set sub-detector */
+  void SetNstb(Int_t nstb) { mNstb = nstb; }
+  /** Set cluster category (see EClusterCategory) */
+  void SetCatag(Int_t catag) { mCatag = catag; }
+  /** Set number of towers (hits) in this cluster */
+  void SetNumbTower(Int_t numbTower) { mNumbTower = numbTower; }
+  /** Set number of points/photons forming this cluster */
+  Bool_t SetNphoton(Int_t nPhoton);
+  /** Set total cluster energy */
+  void SetClusterEnergy(Float_t energy) { mEnergy = energy; }
+  /** Set cluster mean x in local grid coordinates */
+  void SetX0(Float_t x0) { mX0 = x0; }
+  /** Set cluster mean y in local grid coordinates */
+  void SetY0(Float_t y0) { mY0 = y0; }
+  /** Set maximum 2nd moment */
+  void SetSigmaMax(Float_t sigmaMax) { mSigmaMax = sigmaMax; }
+  /** Set minimum 2nd moment */
+  void SetSigmaMin(Float_t sigmaMin) { mSigmaMin = sigmaMin; }
+  /** Set chi2 / ndf for 1-photon fit */
+  void SetChi2NdfPh1(Float_t chi2ndfph1) { mChi2NdfPh1 = chi2ndfph1; }
+  /** Set chi2 / ndf for 2-photon fit */
+  void SetChi2NdfPh2(Float_t chi2ndfph2) { mChi2NdfPh2 = chi2ndfph2; }
+  /** Set cluster ID */
+  void SetClusterId(Float_t cluid) { mCluId = cluid; }
+  /** Set cluster four-momentum (px, py, pz, E) */
+  void SetFourMomentum(TLorentzVector p4) { mFourMomentum = p4; }
+  /** Towers/hits in this cluster */
   StPtrVecFmsHit& hits() { return mHits; }
-	StPtrVecFmsPoint& points() { return mPhotons; }
+  /** Points/photons forming this cluster */
+  StPtrVecFmsPoint& points() { return mPhotons; }
 
-protected:
-	
-	Int_t		mNstb;			// Nstb starts from 1
-	Int_t		mCatag  ;               // catagory of cluster (1: 1-photon,  2: 2-photon,  0: could be either 1- or 2-photon)
-	Int_t		mNumbTower;             // number of non_zero towers in the cluster
-	Int_t		mNphoton;               // number of photons contained in this cluster: ( nPhoton <= MAX_PHOTON_PER_CLUSTER ! )
-	Float_t		mEnergy ;               // total energy  contained in this cluster (0th moment)
-	Float_t		mX0     ;               // mean x ("center of gravity") in local grid coordinate (1st moment)
-	Float_t		mY0     ;               // mean y ("center of gravity") in local grid coordinate (1st moment)     
-	Float_t		mSigmaMax;              // maximum 2nd moment (along major axis)
-	Float_t		mSigmaMin;              // minimum 2nd moment
-	Float_t		mChi2NdfPh1;            // chi2 / ndf for 1-photon fit
-	Float_t		mChi2NdfPh2;            // chi2 / ndf for 2-photon fit
-	Int_t		mCluId     ;            // eventwise cluster id, also include nstb info.
-
-	TLorentzVector  mFourMomentum;		// cluster four momentum;
-
-	StPtrVecFmsPoint mPhotons;
-						//fitted points (photons) in the cluster
-	StPtrVecFmsHit mHits;  // Pointers 
-
-						//an array of tower hits of the current cluster
-	ClassDef(StFmsCluster,1)
+ protected:
+  Int_t mNstb;  ///< Nstb starts from 1
+  Int_t mCatag;  ///< catagory of cluster (see EClusterCategory)
+  Int_t mNumbTower;  ///< Number of non_zero towers in the cluster
+  Int_t mNphoton;  ///< Number of photons contained in this cluster
+  Float_t mEnergy;  ///< Total energy  contained in this cluster (0th moment)
+  Float_t mX0;  ///< Mean x ("center of gravity") in local grid coordinate
+                ///< (1st moment)
+  Float_t mY0;  ///< Mean y ("center of gravity") in local grid coordinate
+                ///< (1st moment)
+  Float_t mSigmaMax;  ///< Maximum 2nd moment (along major axis)
+  Float_t mSigmaMin;  ///< Minimum 2nd moment
+  Float_t mChi2NdfPh1;  ///< chi2 / ndf for 1-photon fit
+  Float_t mChi2NdfPh2;  ///< chi2 / ndf for 2-photon fit
+  Int_t mCluId;  ///< Eventwise cluster id, also include nstb info.
+  TLorentzVector mFourMomentum;  ///< Cluster four momentum;
+  StPtrVecFmsPoint mPhotons;  ///< Tower hits of the current cluster
+  StPtrVecFmsHit mHits;  ///< Fitted points (photons) in the cluster
+  ClassDef(StFmsCluster, 1)
 };
-
-ostream& operator<<(ostream&, const StFmsCluster&);
 
 #endif
