c81758ee950193ff80c3774e55a2944ced1d321e
tomBurton83 <tom_burton83@hotmail.com>
2014-05-07 16:06:51 -0400

    Simplify bfc script to minimise detectors

--- bfc.sh
+++ bfc.sh
@@ -3,4 +3,4 @@
 # Run BFC macro
 # Chain options are SL14a with ry2013_2 geometry
 # http://www.star.bnl.gov/devcgi/dbProdOptionRetrv.pl
-root4star -q -b bfc.C'(1,"DbV20140222 pp2013a mtd btof fmsDb fmsDat fgt fgtPoint VFPPVnoCTB beamline BEmcChkStat Corr4 OSpaceZ2 OGridLeak3D -hitfilt","st_physics_14158051_raw_1030004.daq")'
+root4star -q -b bfc.C'(1,"DbV20140222 pp2013a fmsDb fmsDat","st_physics_14158051_raw_1030004.daq")'
