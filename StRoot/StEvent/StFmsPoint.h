cbd3adf93eebf63a6f331f20fa4ea7639517eeb1
Thomas Burton <tpb@bnl.gov>
2014-06-16 11:29:59 -0400

    Update StFmsPoint Doxygen comments

--- StRoot/StEvent/StFmsPoint.h
+++ StRoot/StEvent/StFmsPoint.h
@@ -18,61 +18,65 @@
 
 class StFmsCluster;
 
-/** Represents a point (photon etc) fitted to a cluster of FMS towers */
+/** Represents a "point" (photon etc) fitted to a cluster of FMS towers. */
 class StFmsPoint : public StObject {
  public:
-  /** Constructor */
+  /** Constructor. */
   StFmsPoint();
   // Use default copy constructor and assignment operator
-  /** Destructor */
+  /** Destructor. */
   ~StFmsPoint();
-  /** Sub-detector in which the point was reconstructed */
+  /**
+   Sub-detector in which the point was reconstructed.
+
+   ID numbers are defined the STAR database.
+   */
   UShort_t detectorId() const { return mDetectorId; }
-  /** Energy of the point in GeV */
+  /** Energy of the point in GeV. */
   Float_t energy() const { return mEnergy; }
-  /** x position in cm at which point intersects the sub-detector */
+  /** x position in cm at which point intersects the sub-detector. */
   Float_t x() const { return mX; }
-  /** y position in cm at which point intersects the sub-detector */
+  /** y position in cm at which point intersects the sub-detector. */
   Float_t y() const { return mY; }
-  /** ID of the point in the current event */
+  /** ID of the point in the current event. */
   Int_t id() const { return mId; }
-  /** Parent cluster of the photon */
+  /** Parent cluster of the photon. */
   StFmsCluster* cluster() { return mCluster; }
-  /** Parent cluster of the photon */
+  /** Parent cluster of the photon. */
   const StFmsCluster* cluster() const { return mCluster; }
-  /** ID of the parent cluster containing this point */
+  /** ID of the parent cluster containing this point. */
   Int_t parentClusterId() const { return mParentClusterId; }
-  /** Number of points in the parent cluster */
+  /** Number of points in the parent cluster. */
   Int_t nParentClusterPhotons() const { return mNParentClusterPhotons; }
-  /** 4-momentum of the point */
+  /** 4-momentum of the point (px, py, pz, E). */
   TLorentzVector fourMomentum() const { return mFourMomentum; }
-  /** Set the sub-detector in which the point was reconstructed */
+  /** Set the sub-detector in which the point was reconstructed. */
   void setDetectorId(UShort_t detector) { mDetectorId = detector; }
-  /** Set the energy of the point in GeV */
+  /** Set the energy of the point in GeV. */
   void setEnergy(Float_t energy) { mEnergy = energy; }
-  /** Set the x position in cm at which point intersects the sub-detector */
+  /** Set the x position in cm at which point intersects the sub-detector. */
   void setX(Float_t xpos) { mX = xpos; }
-  /** Set the y position in cm at which point intersects the sub-detector */
+  /** Set the y position in cm at which point intersects the sub-detector. */
   void setY(Float_t ypos) { mY = ypos; }
-  /** Set the ID of the point in the current event */
+  /** Set the ID of the point in the current event. */
   void setId(Int_t phid) { mId = phid; }
-  /** Set the parent cluster of the photon */
+  /** Set the parent cluster of the photon. */
   void setCluster(StFmsCluster* cluster) { mCluster = cluster; }
-  /** Set the ID of the parent cluster containing this point */
+  /** Set the ID of the parent cluster containing this point. */
   void setParentClusterId(Int_t cluid) { mParentClusterId = cluid; }
-  /** Set the number of points in the parent cluster */
+  /** Set the number of points in the parent cluster. */
   void setNParentClusterPhotons(Int_t nclph) { mNParentClusterPhotons = nclph; }
-  /** Set the 4-momentum of the point */
+  /** Set the 4-momentum of the point (px, py, pz, E). */
   void setFourMomentum(const TLorentzVector& p4) { mFourMomentum = p4; }
 
  protected:
   UShort_t mDetectorId;  ///< Detector starts from 1
-  Float_t mEnergy;  ///< fitted energy
-  Float_t mX;  ///< fitted(relative) x-position
-  Float_t mY;  ///< fitted(relative) y-position
-  Int_t mId;  ///< photon id within event, also include det12 info
-  Int_t mParentClusterId;  ///< id of the parent cluster
-  Int_t mNParentClusterPhotons;  ///< # of photons in by the parent cluster
+  Float_t mEnergy;  ///< Fitted energy
+  Float_t mX;  ///< Fitted x-position
+  Float_t mY;  ///< Fitted y-position
+  Int_t mId;  ///< Photon ID within event
+  Int_t mParentClusterId;  ///< ID of the parent cluster within event
+  Int_t mNParentClusterPhotons;  ///< Number of photons in the parent cluster
   StFmsCluster* mCluster;  //!< Parent cluster of this photon
   TLorentzVector mFourMomentum;  ///< Photon 4-momentum
   ClassDef(StFmsPoint, 1)
